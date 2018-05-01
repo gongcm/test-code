066b374ec915622cd1fd7156374460e68e1160cb
edenCode <gcm1994g@163.com>
2018-05-01 23:30:30 +0800

    files commit

---
+++ python-test/string-test.py
@@ -0,0 +1,32 @@
+
+str = 'Hello world'
+
+print ("字符串长度 : %d" %(len(str)))
+print("字符串的长度 444：",len(str))
+print (str)
+print (str[0])
+print (str[1:5])
+print (str[:len(str)])
+print (str[1:]*3)
+print (str[1:]*5)
+
+print ('字符串拼接')
+
+print ("Hello" + "world")
+
+
+#print （"python :  str.join  Test"）
+str1 = "-"
+
+print (str1.join(str))
+
+
+list = [1,2,3,4]
+
+
+for a in str :
+    print ("当前字母：",a)
+
+n = 0
+for s in list :
+    print ("list[%d] :%d" %(n++,s));
---
+++ python-test/test.py
@@ -0,0 +1,12 @@
+#导入cv模块
+import cv2 as cv
+#读取图像，支持 bmp、jpg、png、tiff 等常用格式
+img = cv.imread("Lena.jpg")
+#创建窗口并显示图像
+cv.namedWindow("Image")
+cv.imshow("Image",img)
+cv.waitKey(0)
+#释放窗口
+cv2.destroyAllWindows()
+
+cv.consloe("Log test\n")
